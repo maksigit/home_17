8b58ef74b87b1f209363925707da32593d82dd4e
maksigit <maksi123p@gmail.com>
2017-02-18 18:57:07 +0200

    Form

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head></head>
+<body>
+
+<form action="#" name="o_sebe" method="post">
+    <fieldset>
+        <legend><b>Коротко о себе</b></legend>
+        <ul class="header">
+            <li>
+                <label for="username">Имя:</label>
+                <input type="text" name="name" id="username">
+            </li>
+            <li>
+                <label for="surname">Фамилия:</label>
+                <input type="text" name="name" id="surname">
+            </li>
+            <li>
+                Пол:
+                <input type="radio" name="radio_p" value="1" checked="checked" id="kind_m">
+                <label for="kind_m">мужской</label>
+                <input type="radio" name="radio_p" value="2" id="kind_j">
+                <label for="kind_j">женский</label>
+            </li>
+            <li>
+                <label for="old">Возраст:</label>
+                <input type="text" name="name" id="old"> лет
+            </li>
+        </ul>
+</form>
+</body>
+</html>
+
---
+++ send.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_POST["send"])){
+    print_r($_POST);
+
+}
+
+
+/**
+ * Created by PhpStorm.
+ * User: АСУС
+ * Date: 15.02.2017
+ * Time: 12:22
+ */
